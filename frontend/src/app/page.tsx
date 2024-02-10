537ab538f9d4d47bbb00a0b3b665c5fd9141a92a
Huzaifa <96327834+nashraaijaz@users.noreply.github.com>
2024-02-10 18:10:59 +0500

    update

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,8 @@
 "use client";
 import HeaderSection from "@/components/headerSection";
 import HeroSection from "@/components/heroSection";
-import VideoGeneratedSection from'@/components/VideoGeneratedSection'
+import VideoGeneratedSection from "@/components/VideoGeneratedSection";
+import AuthModal from "../components/AuthModal";
 import Image from "next/image";
 import { useState } from "react";
 
@@ -9,12 +10,20 @@ export default function Home() {
   const [data, setData] = useState(false)
   return (
     <main>
-      <div className="">
-        {/* <HeaderSection /> */}
+      {/* <div className="">
+        <HeaderSection />
         <HeroSection />
-      </div> */}
-      {/* bh */}
+      </div>  */}
+      <button
+        onClick={() => setData(!data)}
+        className="border rounded-full px-8 py-3 text-2xl bg-blue-500 hover:bg-blue-800 text-white my-3 text-center"
+      >
+        click here
+      </button>
+      <AuthModal open={data} setOpen={setData} />
       <VideoGeneratedSection />
-      </>  
+    
+      
+    </main>
   );
 }
