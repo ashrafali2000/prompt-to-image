689d8cb750759416282c9d8bf51539679aa5f5be
ashrafali2000 <ali4282271@gmail.com>
2024-02-10 19:31:12 +0500

    add integration

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,28 +1,11 @@
 import HeaderSection from "@/components/headerSection";
-import HeroSection from "@/components/heroSection";
-import VideoGeneratedSection from "@/components/VideoGeneratedSection";
-import AuthModal from "../components/AuthModal";
-import Image from "next/image";
-import { useState } from "react";
 
 export default function Home() {
-  const [data, setData] = useState(false)
   return (
     <main>
       <div className="">
-        {/* <HeaderSection /> */}
-        <HeroSection />
-      </div>  */}
-      <button
-        onClick={() => setData(!data)}
-        className="border rounded-full px-8 py-3 text-2xl bg-blue-500 hover:bg-blue-800 text-white my-3 text-center"
-      >
-        click here
-      </button>
-      <AuthModal open={data} setOpen={setData} />
-      <VideoGeneratedSection />
-    
-      
+        <HeaderSection />
+      </div>
     </main>
   );
 }
